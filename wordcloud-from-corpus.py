29e43114cb960006241eae1ae625174e7d3bbfd3
johnhadaway <johnhadaway8@gmail.com>
2022-11-09 11:15:54 +0200

    added .txt file support

--- wordcloud-from-corpus.py
+++ wordcloud-from-corpus.py
@@ -9,9 +9,12 @@ def getPOS(conllu, posList):
     words = []
     for sentence in conllu:
         for token in sentence:
-            if token.upos in posList:
+            if token.upos in posList and token.form != None:
                 words.append(token.form.lower())
     return words
+
+def combineLists(*args):
+    return list(set([item for sublist in args for item in sublist]))
 
 def diff(x, y):
     return list(set(x) - set(y))
@@ -25,11 +28,19 @@ def getWordsFromPDF(pdf_path):
                 words.append(word)
     return [word.lower() for word in words]
 
+def getWordsFromText(text_path):
+    words = []
+    with open(text_path, "r") as f:
+        for line in f:
+            for word in line.split():
+                words.append(word)
+    return [word.lower() for word in words]
+
 def countTokens(tokens, words):
     counts = []
     for token in tokens:
         counts.append(words.count(token))
-    return pandas.DataFrame({'Token': [token.capitalize() for token in tokens], 'Count': counts}).drop(index=pandas.np.where(pandas.np.array(counts) == 0)[0])
+    return pandas.DataFrame({'Token': [token.capitalize() for token in tokens], 'Count': counts})[lambda x: x['Count'] != 0]
 
 def generateWordCloud(df, colormap = 'viridis'):
     stopwords = set(STOPWORDS)
@@ -39,21 +50,20 @@ def generateWordCloud(df, colormap = 'viridis'):
     plt.axis("off")
     plt.tight_layout(pad = 0)
     plt.show()
-    return plt
-    
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog='wordCloudFromCorpus')
     parser.add_argument('--conllu', 
-        help='The path to conllu file', 
+        help='The path to conllu file.', 
         required=True)
-    parser.add_argument('--pdf', 
-        help='The path to pdf file', 
+    parser.add_argument('--file', 
+        help='The path to either the .pdf or .txt file',
         required=True)
     parser.add_argument('--posInclude', 
-        help='The part of speech to be extracted from conllu file. For more than part of speech, wite as a delimited list', 
+        help='The part of speech to be extracted from conllu file. For more than part of speech, wite as a comma-delimited list', 
         required=True)
     parser.add_argument('--posExclude', 
-        help='The part of speech to be extracted from conllu file. For more than part of speech, wite as a delimited list', 
+        help='The part of speech to be extracted from conllu file. For more than part of speech, wite as a comma-delimited list', 
         required=False)
     parser.add_argument('--colormap',
         help='The matplotlib colormap to be used for the word cloud',
@@ -61,13 +71,25 @@ if __name__ == '__main__':
         required=False)
     args = parser.parse_args()
 
+    if args.file.endswith('.pdf'):
+        words = getWordsFromPDF(args.file)
+    elif args.file.endswith('.txt'):
+        words = getWordsFromText(args.file)
+    else:
+        print('This currently must be either .pdf or .txt')
+        exit()
+
     conllu = pyconll.load_from_file(args.conllu)
     list1 = getPOS(conllu, [element for element in args.posInclude.split(',')])
-    list2 = getPOS(conllu, [element for element in args.posExclude.split(',')])
-    list1_exclude_2 = diff(list1, list2)
-    words = getWordsFromPDF(args.pdf)
+    if args.posExclude:
+        list2 = getPOS(conllu, [element for element in args.posExclude.split(',')])
+        list1_exclude_2 = diff(list1, list2)
+        df = countTokens(list1_exclude_2, words)
+    else:
+        df = countTokens(list1, words)
+    
+    generateWordCloud(df, args.colormap)
 
     df = countTokens(list1_exclude_2, words)
     print(df.sort_values(by='Count', ascending=False))
-    df.to_csv(f'./output-data/csv/{args.pdf.split("/")[-1].split(".")[0]}.csv', index=False)
-    plt = generateWordCloud(df, colormap = args.colormap)
+    df.to_csv(f'./output-data/csv/{args.file.split("/")[-1].split(".")[0]}.csv', index=False)
